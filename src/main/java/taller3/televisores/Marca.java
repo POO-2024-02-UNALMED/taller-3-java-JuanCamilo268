54be7adb9c7146ca2b23a9cce9217f692b50f453
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-14 14:00:38 -0500

    Update Marca.java

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -1,4 +1,15 @@
 package taller3.televisores;
 
 public class Marca {
+    private String nombre;
+    public Marca (String nombre) {
+        this.nombre = nombre;
+    }
+    public void setNombre(String Nnombre){
+        this.nombre = Nnombre;
+    }
+    public String getNombre(){
+        String n = this.nombre;
+        return nombre;
+    }
 }
