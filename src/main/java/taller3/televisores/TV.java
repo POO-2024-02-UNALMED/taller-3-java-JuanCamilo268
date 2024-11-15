86e4578f6e4c52253d29581f9e6336ab9dad6f92
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-15 15:26:10 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -10,83 +10,107 @@ public class TV {
     private int volumen = 1;
     private Control control;
 
-    public TV (Marca marca, boolean estado){
+    public TV(Marca marca, boolean estado) {
         this.marca = marca;
         this.estado = estado;
         numTV += 1;
     }
-    public void setMarca(Marca Nmarca){
+
+    public void setMarca(Marca Nmarca) {
         marca = Nmarca;
     }
-    public Marca getMarca(){
+
+    public Marca getMarca() {
         return marca;
     }
-    public void setCanal(int Ncanal){
+
+    public void setCanal(int Ncanal) {
         if ((Ncanal >= 1) && (Ncanal <= 120)) {
             canal = Ncanal;
         }
     }
-    public int getCanal(){
+
+    public int getCanal() {
         return canal;
     }
-    public void setVolumen(int Nvolumen){
+
+    public void setVolumen(int Nvolumen) {
         if ((Nvolumen >= 0) && (Nvolumen <= 7)) {
             volumen = Nvolumen;
+        }
     }
+
     public int getVolumen() {
         return volumen;
     }
-    public void setPrecio(int Nprecio){
+
+    public void setPrecio(int Nprecio) {
         precio = Nprecio;
     }
-    public int getPrecio(){
+
+    public int getPrecio() {
         return precio;
     }
-    public void setControl(Control Ncontrol){
+
+    public void setControl(Control Ncontrol) {
         control = Ncontrol;
     }
-    public Control getControl(){
+
+    public Control getControl() {
         return control;
     }
+
     public void turnOn() {
-        if (!estado){
+        if (!estado) {
             estado = true;
         }
     }
+
     public void turnOff() {
-        if (estado){
+        if (estado) {
             estado = false;
         }
     }
+
     public boolean getEstado() {
         return estado;
     }
+
     public void canalUp() {
-        if (estado){
-            if (canal < 120){
+        if (estado) {
+            if (canal < 120) {
                 canal += 1;
             }
         }
     }
+
     public void canalDown() {
-        if (estado){
-            if (canal > 1){
+        if (estado) {
+            if (canal > 1) {
                 canal -= 1;
             }
         }
     }
+
     public void volumenUp() {
-        if (estado){
-            if (volumen < 7){
+        if (estado) {
+            if (volumen < 7) {
                 volumen += 1;
             }
         }
     }
+
     public void volumenDown() {
-        if (estado){
-            if (volumen > 0){
+        if (estado) {
+            if (volumen > 0) {
                 volumen -= 1;
             }
         }
+    }
+    public static void setNumTV(int n) {
+        numTV = n;
+    }
+    public static int getNumTV() {
+        return numTV;
     }
 }
